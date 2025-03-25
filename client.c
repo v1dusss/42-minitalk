7a98f16b3479bd57c91b176a44ba71e5a212d95d
Vidus Sivanathan <148643381+v1dusss@users.noreply.github.com>
2025-03-25 18:15:01 +0100

    Add get_pid function to validate and convert string to PID (pid parsing)

--- client.c
+++ client.c
@@ -6,7 +6,7 @@
 /*   By: vsivanat <vsivanat@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/10 13:58:18 by vsivanat          #+#    #+#             */
-/*   Updated: 2024/03/14 17:38:42 by vsivanat         ###   ########.fr       */
+/*   Updated: 2025/03/25 18:13:06 by vsivanat         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -53,6 +53,24 @@ void	received_message(int sig)
 	}
 }
 
+int	get_pid(char *str)
+{
+	long	pid;
+	int		i;
+
+	i = -1;
+	pid = 0;
+	while (str[++i] != '\0')
+	{
+		if (!ft_isdigit(str[i]))
+			return (-1);
+		pid = pid * 10 + (str[i] - '0');
+		if (pid > INT_MAX)
+			return (-1);
+	}
+	return ((int)pid);
+}
+
 int	main(int argc, char **argv)
 {
 	int		server_pid;
@@ -62,7 +80,9 @@ int	main(int argc, char **argv)
 		return (ft_printf("Usage: %s <server_pid> <message>\n", argv[0]));
 	signal(SIGUSR1, received_message);
 	signal(SIGUSR2, received_message);
-	server_pid = ft_atoi(argv[1]);
+	server_pid = get_pid(argv[1]);
+	if (server_pid == -1)
+		return (ft_printf("Invalid PID\n"));
 	message = argv[2];
 	send_message(message, server_pid);
 	ft_printf("Waiting for server response.\n");
